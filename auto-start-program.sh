09f13a1549b01c801cd81664e8deb216c097ba92
kim soohyun <soohyunkim950921@gmail.com>
2019-09-10 14:32:58 +0900

    add tor crawler

---
+++ auto-start-program.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup /home/kimsoohyun/00-tor/02-data/00-tor_crawl_data/INPUT/TOR/tor > /dev/null 2>&1 &
+python3 tor_crawler.py -d /home/kimsoohyun/00-tor/02-data/00-tor_crawl_data/ -t 120
+
