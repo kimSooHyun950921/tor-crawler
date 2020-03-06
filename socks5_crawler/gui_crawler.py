42ca830cea4e5bbcc471eec2e0ea9f6c50b1b254
munhyunsu <munhyunsu@gmail.com>
2020-03-06 17:25:04 +0900

    GUI

--- socks5_crawler/gui_crawler.py
+++ socks5_crawler/gui_crawler.py
@@ -1,19 +1,24 @@
 import os
+import sys
 import csv
 import time
+import hashlib
 
+import psutil
 from tbselenium.tbdriver import TorBrowserDriver
 from selenium.common.exceptions import WebDriverException as WebDriverException
 
+FIELD_TIME = ['Type', 'Time', 'Name', 'Address', 'ConvertedAddress',
+              'ResponseTime']
+FLAGS = None
+_ = None
 
-addrpath = os.path.abspath(os.path.expanduser('./test_address.csv'))
-opath = os.path.abspath(os.path.expanduser('./gui_output'))
-mpath = os.path.abspath(os.path.expanduser('./gui.csv'))
-driverpath = os.path.abspath(os.path.expanduser('/home/harny/.local/share/tor-browser_en-US'))
 
-os.makedirs(opath, exist_ok=True)
-
-driver = TorBrowserDriver(driverpath)
+def check_process(psname='tor'):
+    for proc in psutil.process_iter():
+        if proc.name() == psname:
+            return True
+    return False
 
 
 def get_address(path):
@@ -22,19 +27,85 @@ def get_address(path):
         for row in reader:
             yield row['Name'], row['Address']
 
-mf = open(mpath, 'w')
-writer = csv.writer(mf)
 
-for name, addr in get_address(addrpath):
-    ss = time.time()
-    driver.get(addr)
-    data = driver.page_source
-    ofile = os.path.join(opath, f'{name}.html')
-    with open(ofile, 'w') as f:
-        f.write(data)
-    ee = time.time()
-    writer.writerow([name, addr, ee-ss])
-    print(f'Done {name}')
+def get_driver():
+    driver = TorBrowserDriver(FLAGS.driver)
+    return driver
 
-mf.close()
-driver.close()
+
+def convert_addr(value):
+    return hashlib.md5(value.encode('utf-8')).hexdigest()
+
+
+def main():
+    print(f'Parsed arguments: {FLAGS}')
+    print(f'Unparsed arguments: {_}')
+
+    if not check_process():
+        print('Need to running tor for socks5')
+        sys.exit(0)
+
+    print('Start up crawler')
+    # prepare job
+    os.makedirs(FLAGS.output, exist_ok=True)
+    os.makedirs(os.path.join(FLAGS.output, 'html'), exist_ok=True)
+    path_time = os.path.join(FLAGS.output, 'time.csv')
+    if os.path.exists(path_time):
+        file_time = open(path_time, 'a')
+        writer_time = csv.DictWriter(file_time, fieldnames=FIELD_TIME)
+    else:
+        file_time = open(path_time, 'w')
+        writer_time = csv.DictWriter(file_time, fieldnames=FIELD_TIME)
+        writer_time.writeheader()
+
+    # create driver
+    driver = get_driver()
+
+    # do job
+    cnt = 1
+    for name, addr in get_address(FLAGS.input):
+        time_start = time.time()
+        # TODO(LuHa): Need to handling exception
+        driver.get(addr)
+        data = driver.page_source
+        conv_addr = convert_addr(addr)
+        path_html = os.path.join(FLAGS.output, 'html',
+                                 f'{conv_addr}-{int(time_start)}.html')
+        with open(path_html, 'w') as f:
+            f.write(data)
+        time_end = time.time()
+        writer_time.writerow({'Type': 'GUI',
+                              'Time': time_end,
+                              'Name': name,
+                              'Address': addr,
+                              'ConvertedAddress': conv_addr,
+                              'ResponseTime': time_end - time_start})
+        print(f'[{cnt:04d}] {name:^20.20s} {addr:^50.50s}', end='\r')
+        cnt += 1
+    print()
+
+    file_time.close()
+    driver.close()
+    print('Terminate crawler')
+
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--input', type=str, required=True,
+                        help='The addresses formed csv for testing')
+    parser.add_argument('-o', '--output', type=str,
+                        default='./output',
+                        help='The output directory for saving results')
+    parser.add_argument('-d', '--driver', type=str, required=True,
+                        help='The path for tor browser')
+    FLAGS, _ = parser.parse_known_args()
+
+    # path preprocessing
+    FLAGS.input = os.path.abspath(os.path.expanduser(FLAGS.input))
+    FLAGS.output = os.path.abspath(os.path.expanduser(FLAGS.output))
+    FLAGS.driver = os.path.abspath(os.path.expanduser(FLAGS.driver))
+
+    main()
